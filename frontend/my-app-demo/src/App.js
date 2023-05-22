e1f2a54f7aa06fb0e2e0ee4649086755bbcb07e2
osalomon89 <ext_ossalomo@mercadolibre.com>
2023-05-22 11:49:44 -0300

    frontend repo

---
+++ frontend/my-app-demo/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FilterableProductTable from "./Product/FilterableProductTable";
+
+function App() {
+  return (
+    <div className="App">
+      <FilterableProductTable />
+    </div>
+  );
+}
+
+export default App;
